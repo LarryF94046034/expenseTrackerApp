1305ffa485c7ddd4b969160715aefec150b34d0e
larry <kaihsun_chen@9splay.com>
2026-02-10 18:08:31 +0800

    10 TextEditingController 111. Letting Flutter do the Work with TextEditingController
    改為使用內建TextEditing Controller

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,11 +10,7 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
-
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
-  }
+  final _titleController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +19,7 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(label: Text('Title')),
           ),
@@ -31,7 +27,7 @@ class _NewExpenseState extends State<NewExpense> {
             children: [
               ElevatedButton(
                 onPressed: () {
-                  print(_enteredTitle);
+                  print(_titleController.text);
                 },
                 child: const Text('Save Expense'),
               ),
