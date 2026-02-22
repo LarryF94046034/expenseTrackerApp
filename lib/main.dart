72ccb8b3fa505613e3683bc29341ab8e6fc30e4e
larry <kaihsun_chen@9splay.com>
2026-02-22 16:02:06 +0800

    18 Setting & Using a Color Scheme 125. Flutter & Material 3 126. Getting Started with Theming 127. Setting & Using a Color Scheme
    設mainapp color theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,23 @@ import 'package:expenseTrackerApp/styled_text.dart';
 //   runApp(MyApp());
 // }
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp(MaterialApp(theme: ThemeData(useMaterial3: true), home: Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: const Expenses(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
